369000ebf299ca106318543d2c1ac61f59b5814b
clare72965 <hung72965@gmail.com>
2021-02-08 13:48:50 +0800

    add display data

--- images.js
+++ images.js
@@ -29,22 +29,26 @@ import vehicle_disinfection03 from "./public/asset/img/display/image/vehicle_dis
 import spray_motor_group01 from "./public/asset/img/display/image/spray_motor_group01.jpg";
 import spray_motor_group02 from "./public/asset/img/display/image/spray_motor_group02.jpg";
 import spray_motor_group03 from "./public/asset/img/display/image/spray_motor_group03.jpg";
-
+import spray_motor_group04 from "./public/asset/img/display/image/spray_motor_group04.jpg";
 
 import flower_greenhouse_video01 from "./public/asset/img/display/video/flower_greenhouse_video01.mp4";
 import flower_greenhouse_video02 from "./public/asset/img/display/video/flower_greenhouse_video02.mp4";
 import flower_greenhouse_video03 from "./public/asset/img/display/video/flower_greenhouse_video03.mp4";
 import duck_house_video from "./public/asset/img/display/video/duck_house_video.mp4";
 import chicken_coop_video from "./public/asset/img/display/video/chicken_coop_video.mp4";
+import chicken_coop_video2 from "./public/asset/img/display/video/chicken_coop_video2.mp4";
 import spray_video from "./public/asset/img/display/video/spray_video.mp4";
 import personnel_disinfection_video from "./public/asset/img/display/video/personnel_disinfection_video.mp4";
 import cowshed_video from "./public/asset/img/display/video/cowshed_video.mp4";
+
+
 
 import flower_greenhouse_poster_video01 from "./public/asset/img/display/video/flower_greenhouse_poster_video01.jpg"
 import flower_greenhouse_poster_video02 from "./public/asset/img/display/video/flower_greenhouse_poster_video02.jpg"
 import flower_greenhouse_poster_video03 from "./public/asset/img/display/video/flower_greenhouse_poster_video03.jpg"
 import duck_house_poster_video from "./public/asset/img/display/video/duck_house_poster_video.jpg"
 import chicken_coop_poster_video from "./public/asset/img/display/video/chicken_coop_poster_video.jpg"
+import chicken_coop_poster_video2 from "./public/asset/img/display/video/chicken_coop_poster_video2.jpg";
 import spray_poster_video from "./public/asset/img/display/video/spray_poster_video.jpg"
 import personnel_disinfection_poster_video from "./public/asset/img/display/video/personnel_disinfection_poster_video.jpg"
 import cowshed_poster_video from "./public/asset/img/display/video/cowshed_poster_video.jpg"
@@ -54,14 +58,14 @@ const images = {
     Logo: { src: logo, alt: "嘉藝造霧工程行" },
     Banner: { src: banner, alt: "banner" },
     Home_ServeItemImg: [
-        { src: service01, alt: "畜牧消毒降溫" },
-        { src: service02, alt: "溫室加濕降溫" },
-        { src: service03, alt: "車輛消毒系統" }
+        { src: service01, alt: "嘉藝造霧-畜牧消毒降溫" },
+        { src: service02, alt: "嘉藝造霧-溫室加濕降溫" },
+        { src: service03, alt: "嘉藝造霧-車輛消毒系統" }
     ],
     Home_MotorMixImg: [
-        { src: motorMix01, alt: "噴霧馬達組" },
-        { src: motorMix02, alt: "噴霧馬達組" },
-        { src: motorMix03, alt: "噴霧馬達組" }
+        { src: motorMix01, alt: "嘉藝造霧-噴霧馬達組" },
+        { src: motorMix02, alt: "嘉藝造霧-噴霧馬達組" },
+        { src: motorMix03, alt: "嘉藝造霧-噴霧馬達組" }
     ],
     Home_DisplayImg: [
         { src: display_spray, alt: "噴霧" },
@@ -84,13 +88,15 @@ const images = {
         { src: vehicle_disinfection03, alt: "車輛消毒" },
         { src: spray_motor_group01, alt: "噴霧馬達組" },
         { src: spray_motor_group02, alt: "噴霧馬達組" },
-        { src: spray_motor_group03, alt: "噴霧馬達組" }],
+        { src: spray_motor_group03, alt: "噴霧馬達組" },
+        { src: spray_motor_group04, alt: "噴霧馬達組" }],
     Achieve_DisplayVideo: [
         { src: flower_greenhouse_video01, poster: flower_greenhouse_poster_video01 },
         { src: flower_greenhouse_video02, poster: flower_greenhouse_poster_video02 },
         { src: flower_greenhouse_video03, poster: flower_greenhouse_poster_video03 },
         { src: duck_house_video, poster: duck_house_poster_video },
         { src: chicken_coop_video, poster: chicken_coop_poster_video },
+        { src: chicken_coop_video2, poster: chicken_coop_poster_video2 },
         { src: spray_video, poster: spray_poster_video },
         { src: personnel_disinfection_video, poster: personnel_disinfection_poster_video },
         { src: cowshed_video, poster: cowshed_poster_video }
