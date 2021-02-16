c9bbbb8550d08db8c09dd90036077336ae0e1216
clare72965 <hung72965@gmail.com>
2021-02-16 17:09:32 +0800

    edit  description

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Index({ topBtn }) {
       <Head>
         <title>嘉藝造霧工程行</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="嘉藝造霧服務項目: 畜牧消毒降溫、溫室加濕降溫、車輛消毒系統。責任施工，專業設計" />
+        <meta name="description" content="嘉藝造霧-噴霧系統設計、安裝。技術服務項目: 畜牧消毒降溫、溫室加濕降溫、車輛消毒系統。責任施工，專業設計" />
         <meta name="google-site-verification" content="9N9oYTdJ275JTcfxNPjbSDD50557LRp5yzz-0J9sjSw" />
       </Head>
       <Home topBtn={topBtn}></Home>
